4aa4cd481612b1797744e24409d07c7d9acd3749
pratapaditya1997 <pratapaditya1997@gmail.com>
2021-03-28 23:04:11 +0530

    factory method design pattern example added

---
+++ FactoryMethod/src/main/java/Demo.java
@@ -0,0 +1,24 @@
+import factory.Dialog;
+import factory.HtmlDialog;
+import factory.WindowsDialog;
+
+public class Demo {
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+}
